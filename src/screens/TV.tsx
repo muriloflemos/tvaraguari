fd11d0ed4c720d3c41eddaaa5686179307ccf806
Murilo Lemos <murilo.lemos@otus.com>
2022-08-10 22:23:07 -0300

    Release version 1.0

--- src/screens/TV.tsx
+++ src/screens/TV.tsx
@@ -8,10 +8,6 @@ import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { TV_URL } from '../config';
 
 const styles = StyleSheet.create({
-  area: {
-    flex: 1,
-    backgroundColor: '#000',
-  },
   webview: {
     backgroundColor: '#000'
   },
@@ -45,25 +41,19 @@ export function TV({ navigation }: any) {
   }
 
   return (
-    <SafeAreaView style={styles.area}>
-      <VStack flex={1} bg="black">
-        <HStack
-          safeArea
-          bg="black"
-          px="1" 
-          py="3" 
-          justifyContent="space-between" 
-          alignItems="center" 
-          w="100%"
-          h="20"
-        >
-          <IconButton 
-            icon={<FontAwesomeIcon size={22} icon={faChevronLeft} color="#ffffff" />}
-            onPress={onClickBack}
-          />
-        </HStack>
-        {renderWebView()}
-      </VStack>
-    </SafeAreaView>
+    <VStack safeArea flex={1} bg="black">
+      <HStack
+        bg="black"
+        justifyContent="flex-start" 
+        alignItems="center" 
+        w="100%"
+      >
+        <IconButton 
+          icon={<FontAwesomeIcon size={22} icon={faChevronLeft} color="#ffffff" />}
+          onPress={onClickBack}
+        />
+      </HStack>
+      {renderWebView()}
+    </VStack>
   );
 }
